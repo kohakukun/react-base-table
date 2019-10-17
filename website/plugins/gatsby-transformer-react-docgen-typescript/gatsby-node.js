5c976ded0cf5133f86db7567efa30e501ce93141
Galib Hassan <galib.hassan.0@gmail.com>
2019-10-17 15:50:40 +0200

    removing local 'extend-node-type.js', more cleanup

--- website/plugins/gatsby-transformer-react-docgen-typescript/gatsby-node.js
+++ website/plugins/gatsby-transformer-react-docgen-typescript/gatsby-node.js
@@ -1,4 +1,4 @@
 "use strict";
 
 exports.onCreateNode = require(`./on-node-create`).default;
-exports.setFieldsOnGraphQLNodeType = require(`./extend-node-type`).default;
+exports.setFieldsOnGraphQLNodeType = require(`gatsby-transformer-react-docgen/extend-node-type`).default;
